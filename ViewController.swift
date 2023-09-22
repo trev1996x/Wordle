cb12447089f89db9b94a34ba5b4f9d941828389e
trev1996x <115669657+trev1996x@users.noreply.github.com>
2023-09-22 01:32:37 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Wordle
+//
+//  Created by Mari Batilando on 2/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var wordsCollectionView: UICollectionView!
+  @IBOutlet weak var keyboardCollectionView: UICollectionView!
+
+  private var boardController: BoardController!
+  private var keyboardController: KeyboardController!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupNavigationBar()
+
+    boardController = BoardController(collectionView: wordsCollectionView)
+    keyboardController = KeyboardController(collectionView: keyboardCollectionView)
+    /*
+      Exercise 3: Assign a closure to the `didSelectString` property of `keyboardController` (see KeyboardController.swift):
+      
+      This closure takes in a string (the string selected from the keyboard).
+      If the string is equal to the `DELETE_KEY` constant (see Constants.swift), then call the `deleteLastCharacter` method of `boardController`.
+      Else, it should use the `enter` method of `boardController` and pass in the selected string as the argument.
+     */
+    // START YOUR CODE HERE
+//      keyboardController.didSelectString = {
+//          if ( = DELETE_KEY) {
+//              deleteLastCharacter()
+//          }
+//      }
+    // END YOUR CODE HERE
+  }
+}
+
+// I HONESTLY AM SO CONFUSED ON THIS ENTIRE PROGRAM. IT IS WAY HARDER THAN IT SHOULD BE FOR A STARTER PROGRAM FOR LEARNING AN ENTIRE NEW LANGUAGE AND ITS SYNTAX. TURNING IT IN UNFINISHED AND WILL CONTINUE TO WORK ON IT. 
